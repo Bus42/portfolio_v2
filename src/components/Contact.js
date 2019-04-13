e7090271ded76d64c9d4bfcff52f1eecb54c879a
Bus42 <gbrewton1980@gmail.com>
2019-04-13 10:41:33 -0600

    Adds responsive links to Gallery, Certifications

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,14 +11,14 @@ class Contact extends Component {
           logo: <i className="fab fa-github" />
         },
         {
-            name: " freeCodeCamp",
-            url: "https://www.freecodecamp.org/bus42",
-            logo: <i className="fab fa-free-code-camp"></i>
+          name: " freeCodeCamp",
+          url: "https://www.freecodecamp.org/bus42",
+          logo: <i className="fab fa-free-code-camp" />
         },
         {
-            name: " linkedIn",
-            url: "https://www.linkedin.com/in/gregbrewton/",
-            logo: <i className="fab fa-linkedin"></i>
+          name: " linkedIn",
+          url: "https://www.linkedin.com/in/gregbrewton/",
+          logo: <i className="fab fa-linkedin" />
         }
       ]
     };
@@ -34,18 +34,23 @@ class Contact extends Component {
         }}
       >
         {this.state.contacts.map((contact, index) => (
-          <span
-            className="contact"
-            /** <-- use class of 'contact' for responsive styling in App.css */ key={
-              index
-            }
-          >
+          <span key={index}>
             <a style={{ color: "inherit" }} href={contact.url}>
-              {contact.logo}
-              {contact.name}
+              <span>
+                <span className="hide-on-med-only">{contact.logo}</span>{" "}
+                <span className="hide-on-small-only">{contact.name}</span>
+              </span>
             </a>
           </span>
         ))}
+        <span>
+          <i className="far fa-clone hide-on-med-only" />{" "}
+          <span className="hide-on-small-only">Project Gallery</span>{" "}
+        </span>
+        <span>
+          <i className="fas fa-certificate hide-on-med-only" />{" "}
+          <span className="hide-on-small-only">Certifications</span>{" "}
+        </span>
       </div>
     );
   }
