b733a7c47b061cb5dfb276681c33da61a82d6edf
Bus42 <gbrewton1980@gmail.com>
2019-05-11 10:21:18 -0600

    Adds breakpoints for padding

--- src/App.js
+++ src/App.js
@@ -1,50 +1,51 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import Gallery from "./components/Gallery";
-import Certifications from "./components/Certifications";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Footer from './components/Footer'
+import Home from './components/Home'
+import Gallery from './components/Gallery'
+import Certifications from './components/Certifications'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     this.state = {
       touchscreen: false,
-      home: "/",
-      gallery: "/gallery",
-      certifications: "/certifications"
-    };
-  }
-  componentDidMount() {
-    if (window.navigator.maxTouchPoints > 0) {
-      this.setState({ touchscreen: true });
+      home: '/',
+      gallery: '/gallery',
+      certifications: '/certifications'
     }
   }
-  render() {
-    const home = this.state.home;
-    const gallery = this.state.gallery;
-    const certifications = this.state.certifications;
+  componentDidMount () {
+    if (window.navigator.maxTouchPoints > 0) {
+      this.setState({ touchscreen: true })
+    }
+  }
+  render () {
+    const home = this.state.home
+    const gallery = this.state.gallery
+    const certifications = this.state.certifications
     return (
       <div
+        id='app-wrapper'
         style={{
-          display: "flex",
-          flexFlow: "column nowrap",
-          justifyContent: "space-between",
-          minHeight: "100vh"
+          display: 'flex',
+          flexFlow: 'column nowrap',
+          justifyContent: 'space-between',
+          minHeight: '100vh'
         }}
       >
         <Router>
-          <div id="wrapper">
+          <div>
             <Header />
             <Navbar />
             <div
-              className="container"
+              className='container'
               style={{
-                paddingTop: "5em",
-                display: "flex",
-                flexFlow: "column nowrap",
-                justifyContent: "center"
+                paddingTop: '5em',
+                display: 'flex',
+                flexFlow: 'column nowrap',
+                justifyContent: 'center'
               }}
             >
               <Switch>
@@ -57,7 +58,7 @@ class App extends Component {
         </Router>
         <Footer />
       </div>
-    );
+    )
   }
 }
-export default App;
+export default App
