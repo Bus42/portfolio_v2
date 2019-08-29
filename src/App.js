27caf3e124e0027592049adc73cb570d2e919064
Greg Brewton <gbrewton1980@gmail.com>
2019-08-29 13:34:52 -0600

    Moves Footer same depth in Router as Header/Navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
-import Footer from './components/Footer'
-import Home from './components/Home'
-import Gallery from './components/Gallery'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import { certifications } from './components/certificationList'
-import { projects } from './components/projectList'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Gallery from "./components/Gallery";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import { certifications } from "./components/certificationList";
+import { projects } from "./components/projectList";
 const App = () => {
   return (
     <div
-      id='app-wrapper'
+      id="app-wrapper"
       style={{
-        display: 'flex',
-        flexFlow: 'column nowrap',
-        justifyContent: 'space-between',
-        minHeight: '100vh'
+        display: "flex",
+        flexFlow: "column nowrap",
+        justifyContent: "space-between",
+        minHeight: "100vh"
       }}
     >
       <Router>
@@ -23,25 +28,31 @@ const App = () => {
           <Header />
           <Navbar />
           <div
-            className='container'
+            className="container"
             style={{
-              paddingTop: '5em',
-              display: 'flex',
-              flexFlow: 'column nowrap',
-              justifyContent: 'center'
+              paddingTop: "5em",
+              display: "flex",
+              flexFlow: "column nowrap",
+              justifyContent: "center"
             }}
           >
             <Switch>
-              <Route exact path='/' component={Home} />
-              <Route path='/projects' render={(props) => <Gallery list={projects} /> } />
-              <Route path='/certifications' render={(props) => <Gallery list={certifications} /> } />
-              <Route render={() => (<Redirect to='/' />)} />
+              <Route exact path="/" component={Home} />
+              <Route
+                path="/projects"
+                render={props => <Gallery list={projects} />}
+              />
+              <Route
+                path="/certifications"
+                render={props => <Gallery list={certifications} />}
+              />
+              <Route render={() => <Redirect to="/" />} />
             </Switch>
           </div>
+          <Footer />
         </div>
       </Router>
-      <Footer />
     </div>
-  )
-}
-export default App
+  );
+};
+export default App;
